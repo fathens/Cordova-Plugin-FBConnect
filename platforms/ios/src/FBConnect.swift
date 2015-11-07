d494df2834f603d47a716383af489aebefe5dcc8
Kunio Sawatani <devel@fathens.org>
2015-11-07 14:18:21 +0900

    Facebook SDK の初期化コード

--- platforms/ios/src/FBConnect.swift
+++ platforms/ios/src/FBConnect.swift
@@ -2,7 +2,15 @@ import Foundation
 
 @objc(FBConnect)
 class FBConnect: CDVPlugin {
-    func log(command: CDVInvokedUrlCommand) {
-        logmsg(command)
+    override func pluginInitialize() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "finishLaunching", name: UIApplicationDidFinishLaunchingNotification, object: nil);
+    }
+    
+    func finishLaunching(notification: NSNotification) {
+        FBSDKApplicationDelegate.sharedInstance().application(UIApplication.sharedApplication(), didFinishLaunchingWithOptions: notification.userInfo)
+    }
+    
+    override func handleOpenURL(notification: NSNotification) {
+        FBSDKApplicationDelegate.sharedInstance().application(UIApplication.sharedApplication(), openURL: notification.object! as! NSURL, sourceApplication: nil, annotation: nil)
     }
 }
