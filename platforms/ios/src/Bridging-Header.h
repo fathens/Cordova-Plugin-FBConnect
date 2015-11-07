99434c0f4e34d2f0945cea0c9faf594b08e42373
Kunio Sawatani <devel@fathens.org>
2015-11-07 10:02:01 +0900

    Fabric ではなく FBSDK に変更

--- platforms/ios/src/Bridging-Header.h
+++ platforms/ios/src/Bridging-Header.h
@@ -1,3 +1,4 @@
 #import <Cordova/CDV.h>
-#import <Fabric/Fabric.h>
-#import <Crashlytics/Crashlytics.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
