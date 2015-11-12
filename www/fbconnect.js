2a0e42983e10c23588396e4b82ea1738e93fc909
Kunio Sawatani <devel@fathens.org>
2015-11-12 10:22:21 +0900

    renewSystemCredentials を追加

--- www/fbconnect.js
+++ www/fbconnect.js
@@ -2,25 +2,22 @@ var cordova = require("cordova/exec");
 
 var pluginName = "FBConnectPlugin"
 
-var names = [
-"login",
-"getName"
-];
+var names = [ "renewSystemCredentials", "login", "getName" ];
 
 var obj = {};
 
 names.forEach(function(methodName) {
-    obj[methodName] = function() {
-        callback = arguments[0]
-        args = Array.prototype.slice.call(arguments, 1)
-        console.log("Calling " + pluginName + "." + methodName + ": " + args);
+	obj[methodName] = function() {
+		callback = arguments[0]
+		args = Array.prototype.slice.call(arguments, 1)
+		console.log("Calling " + pluginName + "." + methodName + ": " + args);
 
-        cordova(function(result) {
-            callback(null, result);
-        }, function(error) {
-            callback(error);
-        }, pluginName, methodName, args);
-    }
+		cordova(function(result) {
+			callback(null, result);
+		}, function(error) {
+			callback(error);
+		}, pluginName, methodName, args);
+	}
 });
 
 module.exports = obj;
