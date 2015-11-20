5c8a3b88b28ec069ac48cf6124c80add0c4dfce2
Kunio Sawatani <devel@fathens.org>
2015-11-20 11:11:13 +0900

    login を実装

--- platforms/android/src/main/kotlin/FBConnect.kt
+++ platforms/android/src/main/kotlin/FBConnect.kt
@@ -1,8 +1,11 @@
 package org.fathens.cordova.plugin.fbconnect
 
+import com.facebook.CallbackManager
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaPlugin
 import org.json.JSONArray
+import com.facebook.*
+import com.facebook.login.*
 
 public class FBConnect: CordovaPlugin() {
     override fun execute(action: String, data: JSONArray, callbackContext: CallbackContext): Boolean {
@@ -11,7 +14,6 @@ public class FBConnect: CordovaPlugin() {
             val runner = method.invoke(this, data) as (() -> Unit)
             cordova.getThreadPool().execute {
                 runner()
-                callbackContext.success()
             }
             return true
         } catch (e: NoSuchMethodException) {
@@ -19,10 +21,24 @@ public class FBConnect: CordovaPlugin() {
         }
     }
 
-    public fun login(args: JSONArray): (() -> Unit) {
-        val msg = args.getString(0)
+    public fun login(callbackContext: CallbackContext, args: JSONArray): () -> Unit {
         return {
-            println(msg)
+            val msg = args.getString(0)
+            val cm  = CallbackManager.Factory.create()
+            LoginManager.getInstance().registerCallback(cm, object: FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    callbackContext.success()
+                }
+
+                override fun onCancel() {
+                    callbackContext.error("Cancelled")
+                }
+
+                override fun onError(error: FacebookException?) {
+                    throw UnsupportedOperationException()
+                }
+            })
+            LoginManager.getInstance().logInWithReadPermissions(cordova.activity, listOf("public_profile"))
         }
     }
 }
