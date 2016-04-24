6cec927d077b9a0e9ea1886cc3a8ff975f4363ff
Kunio Sawatani <devel@fathens.org>
2016-04-24 19:38:20 +0900

    Format

--- www/fbconnect.js
+++ www/fbconnect.js
@@ -7,16 +7,16 @@ var names = [ "login", "logout", "getToken", "getName" ];
 var obj = {};
 
 names.forEach(function(methodName) {
-	obj[methodName] = function() {
-		callback = arguments[0];
-		args = Array.prototype.slice.call(arguments, 1);
+    obj[methodName] = function() {
+        callback = arguments[0];
+        args = Array.prototype.slice.call(arguments, 1);
 
-		cordova(function(result) {
-			callback(null, result);
-		}, function(error) {
-			callback(error, null);
-		}, pluginName, methodName, args);
-	}
+        cordova(function(result) {
+            callback(null, result);
+        }, function(error) {
+            callback(error, null);
+        }, pluginName, methodName, args);
+    }
 });
 
 module.exports = obj;
